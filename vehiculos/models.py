2cba83971e97d6d61ec5493306c9dc7240439621
JoseJimArg <jose.jimenez.dev@gmail.com>
2021-11-17 21:21:28 -0600

    Agregar unique a la matricula dentro del modelo de vehiculo.

--- vehiculos/models.py
+++ vehiculos/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Vehiculo(models.Model):
     modelo = models.CharField("Modelo", max_length=20)
     marca = models.CharField("Marca", max_length=20)
-    matricula = models.CharField("Matricula", max_length=7)
+    matricula = models.CharField("Matricula", max_length=7, unique=True)
     asientos = models.IntegerField("Asientos")
     descripcion = models.CharField("Descripcion", max_length=500)
     estado = models.BooleanField("Estado", default=True, null=False, blank=False)
