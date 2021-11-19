0cf9ffbe744fd7dcd537464f773f700995de59ca
Oscar Leonardo Corvera Espinosa <35166865@uaz.edu.mx>
2021-11-18 20:16:58 -0600

    Hago unique el correo y teléfono del usuario. Muestro los campos obligatorios y errores en el signup.

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,9 +4,13 @@ from reito import settings
 from cloudinary.models import CloudinaryField
 
 
+User._meta.get_field('email')._unique = True
+User._meta.get_field('email').null = False
+User._meta.get_field('email').blank = False
+
 # Model representing a user of the system.
 class Usuario(User):
-    telefono = models.BigIntegerField()
+    telefono = models.BigIntegerField(unique=True, blank=False, null=False)
     foto = CloudinaryField('image', default=None, blank=True, null=True)
     descripcion = models.CharField(max_length=300, blank=True, null=True)
 
